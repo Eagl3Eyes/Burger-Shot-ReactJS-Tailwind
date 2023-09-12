6325f8e8c73ff455db646b8e3b56d1254b6fee53
Md Tuhin Al Jobayer <tuhinjobayergolap007@gmail.com>
2023-09-12 19:44:26 +0600

    headline added

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navbar from '../navbar/Navbar';
+import Hero from '../hero/Hero';
+import Headline from '../headline/Headline';
 
 const Main = () => {
     return (
         <div>
             <Navbar />
+            <Hero />
+            <Headline />
         </div>
     );
 };
